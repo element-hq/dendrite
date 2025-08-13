c15c827f104e97f47d345c62484d4552423e8e6d
Kegan Dougal <7190048+kegsay@users.noreply.github.com>
2025-08-13 16:08:52 +0100

    Update to latest GMSL; remove debug logs

--- federationapi/internal/perform.go
+++ federationapi/internal/perform.go
@@ -575,7 +575,6 @@ func (r *FederationInternalAPI) SendInvite(
 		"room_version": event.Version(),
 		"destination":  destination,
 	}).Info("Sending invite")
-	logrus.Infof("invite event json %s", string(event.JSON()))
 
 	inviteReq, err := fclient.NewInviteV2Request(event, strippedState)
 	if err != nil {
